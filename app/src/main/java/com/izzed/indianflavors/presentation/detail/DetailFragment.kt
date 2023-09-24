062de6699aa762560e177de2ae4a8ba095cd72e7
Muhammad Izzudin <mohammadzzudin@gmail.com>
2023-09-24 17:15:16 +0700

    finishing feature detail

--- app/src/main/java/com/izzed/indianflavors/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/izzed/indianflavors/presentation/detail/DetailFragment.kt
@@ -16,7 +16,9 @@ class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
 
-
+    private val product: Product? by lazy {
+        DetailFragmentArgs.fromBundle(arguments as Bundle).product
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +26,75 @@ class DetailFragment : Fragment() {
     ): View? {
         binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListener()
+        showProductData()
+        controlTotalItems()
+    }
+
+    private fun controlTotalItems() {
+        var totalItems = 0
+
+        binding.ibMinus.setOnClickListener {
+            if (totalItems > 0) {
+                totalItems--
+                updateTotalItemsText(totalItems)
+                totalPrice(totalItems)
+            }
+        }
+
+        binding.ibPlus.setOnClickListener {
+            totalItems++
+            updateTotalItemsText(totalItems)
+            totalPrice(totalItems)
+        }
+    }
+
+    private fun totalPrice(totalItems: Int) {
+        val price = product?.price?.replace("Rp", "")?.replace(".", "")?.trim()?.toInt()
+        if (price != null) {
+            val totalPrice = price * totalItems
+            val formattedTotalPrice = formatPrice(totalPrice)
+            binding.tvTotalPrice.text = formattedTotalPrice
+        } else {
+            binding.tvTotalPrice.text = "Price not available"
+        }
+    }
+
+    private fun formatPrice(price: Int): String {
+        val formatter = DecimalFormat("#,###")
+        return "Rp " + formatter.format(price)
+    }
+
+    private fun updateTotalItemsText(totalItems: Int) {
+        binding.tvItem.text = totalItems.toString()
+    }
+
+    private fun showProductData() {
+        product?.let { p ->
+            binding.apply {
+                ivProduct.load(p.imgUrl) {
+                    crossfade(true)
+                }
+                tvProductName.text = p.name
+                tvProductPrice.text = p.price
+                tvProductDesc.text = p.desc
+            }
+        }
+    }
+
+    private fun setClickListener() {
+        binding.cvLocation.setOnClickListener{
+            navigateToGoogleMaps()
+        }
+    }
+
+    private fun navigateToGoogleMaps() {
+        val uri = "https://maps.app.goo.gl/h4wQKqaBuXzftGK77"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+        startActivity(intent)
     }
 }
